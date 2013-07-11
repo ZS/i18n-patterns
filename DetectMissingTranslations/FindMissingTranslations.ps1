36e4f1908141d582d0b5f93ac8bfa072f8ab25e6
neclark2 <neclark2@gmail.com>
2013-07-11 07:53:24 -0500

    adding missing translation script

---
+++ DetectMissingTranslations/FindMissingTranslations.ps1
@@ -0,0 +1,39 @@
+﻿param (
+	$resourceFileName = $(throw "must specify a resource file name")
+)
+
+function Get-ScriptDirectory {
+	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
+	Write-Output $(Split-Path $Invocation.MyCommand.Path)
+}
+
+$dir = Get-ScriptDirectory
+[xml] $baseResourceFile = Get-Content -Path $(Join-Path $dir $($resourceFileName + ".resx"))
+$baseStrings = @{}
+$otherResourceFiles = gci $(Join-Path $dir $($resourceFileName + "*.resx"))
+$numMissingTranslations = 0
+
+$baseResourceFile.root.data | %{
+	$baseStrings.Add($_.name, "")
+}
+
+$otherResourceFiles | % {
+	[xml]$file = Get-Content -Path $_
+	$fileName = $_.Name
+	$otherStrings = @{}
+	$file.root.data | %{
+		$otherStrings.Add($_.name, "")
+	}
+	
+	$baseStrings.Keys | % {
+		if(-not $otherStrings.ContainsKey($_)) {
+			echo "$fileName does not contain a translation for '$_' " 
+			$numMissingTranslations++
+		}
+	}
+}
+
+if ($numMissingTranslations -ne 0) {
+	Write-Host "$numMissingTranslations missing translations found...Time to reach out to Language Line!"
+	exit 1
+}
